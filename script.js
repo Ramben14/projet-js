80cd6333aa13ccf205371cf3f023ebeeb4abb67c
Ramben14 <ramziben111@gmail.com>
2023-04-24 19:54:31 +0200

    2 eme commit

---
+++ script.js
@@ -0,0 +1,12 @@
+let button = document.getElementById('button');
+let form = document.getElementById('form');
+let message = document.getElementById('message');
+let link = document.getElementById('link');
+
+
+const secretNumber = Math.floor(Math.random() * 150);
+let attempts = 10; 
+
+button.oneclick = function(){
+    const input = document .getElementById("input"); 
+}
