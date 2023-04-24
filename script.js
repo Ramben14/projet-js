c77cda01058f5804ce0de26f560b42e6bd436fc2
Ramben14 <ramziben111@gmail.com>
2023-04-24 20:38:13 +0200

    cm

--- script.js
+++ script.js
@@ -9,4 +9,20 @@ let attempts = 10;
 
 button.oneclick = function(){
     const input = document .getElementById("input"); 
+    if(input.value != ""){
+
+        if(input.value< secretNumber){
+            message.innerText= "le nombre est plus grand"; + input.value + '';
+            message.style.color = "red";
+        }else if (input.value > secretNumber){
+            message.innerText= "le nombre est plus petit"; + input.value + '';
+            message.style.color = "red";
+        }
+        
+
+    }else {
+        message.innerText = "est vide";
+        message.style.color="red";
+
+    }
 }
