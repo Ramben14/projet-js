f93533c677a5f843fe355f2c7be4622b038deb2e
Ramben14 <ramziben111@gmail.com>
2023-04-25 10:10:42 +0200

    faire des condition

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
-let button = document.getElementById('button')
-let form = document.getElementById('form')
-let message = document.getElementById('message')
-let link = document.getElementById('link')
+let button = document.getElementById('button');
+let form = document.getElementById('form');
+let message = document.getElementById('message');
+let link = document.getElementById('link');
 
 const secretNumber = Math.floor(Math.random() * 100);
 let attempts = 5;
 
 button.onclick = function(){
-    alert('papappa')
+    const input = document.getElementById("input");
+    if(input.value != ""){
+
+        if(input.value < secretNumber){
+            message.innerText = 'le nombre est plus grand ' + input.value ;
+            message.style.color = "#6495ED";
+        }
+
+    }else{
+        message.innerText = "le champs est vide ";
+        message.style.color="red";
+    }
 }
