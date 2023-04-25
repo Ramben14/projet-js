b8d13a87f49bb148ca0634447a554e35934a729f
Ramben14 <ramziben111@gmail.com>
2023-04-25 09:55:38 +0200

    crrer une fonction pour faire des test

--- script.js
+++ script.js
@@ -2,3 +2,10 @@ let button = document.getElementById('button')
 let form = document.getElementById('form')
 let message = document.getElementById('message')
 let link = document.getElementById('link')
+
+const secretNumber = Math.floor(Math.random() * 100);
+let attempts = 5;
+
+button.onclick = function(){
+    alert('papappa')
+}
